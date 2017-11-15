aa67e14b0742bddbece5065e397225dad8c4d559
Reio <reio@heathmont.net>
2017-11-15 18:49:32 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const { HmCrypto } = require('./lib');
+const HmCrypto = require('./lib').HmCrypto;
 
 module.exports = (digestType, privateKey, publicKey) => {
   return new HmCrypto(digestType, privateKey, publicKey);
